5896ebefdada9515907e78826fbf6704e1883b58
We-Gold <we.goldm@gmail.com>
2021-07-22 19:24:38 -0400

    Made sure the offsets round.

--- src/components/robot/limelightOffset.jsx
+++ src/components/robot/limelightOffset.jsx
@@ -8,16 +8,22 @@ export default class LimelightOffset extends Component {
         this.yOffset = "/limelight/yOffset"
     }
 
+    roundTwoDecimals(num) {
+        if(num == undefined || num == null) return ""
+
+        return +num.toFixed(2)
+    }
+
     render() {
         return (
             <div className="col-span-1 p-2 grid grid-cols-2">
                 <div className="col-span-1">
                     <div className="text-left">x Offset:</div>
-                    <div className="text-left text-2xl"><strong>{this.props.nt[this.xOffset]}</strong></div>
+                    <div className="text-left text-2xl"><strong>{this.roundTwoDecimals(this.props.nt[this.xOffset])}</strong></div>
                 </div>
                 <div className="col-span-1">
                     <div className="text-left">y Offset:</div>
-                    <div className="text-left text-2xl"><strong>{this.props.nt[this.yOffset]}</strong></div>
+                    <div className="text-left text-2xl"><strong>{this.roundTwoDecimals(this.props.nt[this.yOffset])}</strong></div>
                 </div>
             </div>
         )
