bed0dcbb57e52a70c19297d3d3edbfab4c2d372b
Krypton Cougars <Krypton Cougars@DESKTOP-KLEB8D3>
2022-08-13 12:03:48 -0400

    Added a shooter map graph.

--- src/components/robot.jsx
+++ src/components/robot.jsx
@@ -1,6 +1,8 @@
 import { Component } from "preact" 
 
 import LimelightOffset from "./robot/limelightOffset"
+import ShootingGraph from "./robot/shootingGraph"
+
 export default class Robot extends Component {
     constructor(props) {
         super(props)
@@ -8,8 +10,9 @@ export default class Robot extends Component {
 
     render() {
         return (
-            <div className="shadow row-span-1 w-10/12 col-span-4 bg-orange rounded-lg p-2 mt-3 mb-3 grid grid-cols-1">
-                <LimelightOffset nt={this.props.nt}/>
+            <div className="shadow row-span-1 w-11/12 col-span-4 bg-orange rounded-lg p-1 mt-3 mb-3 grid grid-cols-1">
+                {/* <LimelightOffset nt={this.props.nt}/> */}
+                <ShootingGraph nt={this.props.nt}/>
             </div>
         )
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,jsx}'],
   darkMode: false,
   theme: {
+    fontSize: {
+      'svg-xs': '.40rem',
+      'svg-s': '.43rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
     rotate: {
       '-180': '-180deg',
       '-90': '-90deg',
