e38190497312ee0ffbd66a2b467ce96d34d87ad5
Krypton Cougars <Krypton Cougars@DESKTOP-KLEB8D3>
2022-09-14 18:45:47 -0400

    Added a ghost to the field widget.

--- src/field.jsx
+++ src/field.jsx
@@ -9,19 +9,49 @@ export default class Field extends Component {
     constructor(props) {
         super(props)
 
-        this.robotXKey = '/Swerve/X'
-        this.robotYKey = '/Swerve/Y'
-        this.robotAngleKey = '/Swerve/Angle'
+        this.robotPoseKey = '/Swerve/robotPose'
+        this.ghostPoseKey = '/Swerve/ghostPose'
+        this.ghostEnableKey = '/Swerve/enableGhostPose'
+
         this.pathPointsKey = '/Swerve/Trajectory'
     }
-    render() {
-        this.robotPose = {
-            x: this.props.nt[this.robotXKey] ?? 0,
-            y: this.props.nt[this.robotYKey] ?? 0,
-            angle: this.props.nt[this.robotAngleKey] ?? 0,
+
+    decodeRobotPoseArray(poseArray) {
+        const arrayOrDefault = poseArray ?? [0, 0, 0]
+        return {
+            x: arrayOrDefault[0],
+            y: arrayOrDefault[1],
+            angle: arrayOrDefault[2]
         }
-        this.pathPoints = this.props.nt[this.pathPointsKey] ?? []
-        // Correct data format: ['0.5,1', '1,3', '1.5,4', '4,4.5']
+    }
+
+    createRobotSvg(robotPose, isGhost) {
+        return <>
+            <rect 
+                stroke={!isGhost ? "white" : "rgb(150, 150, 150)"} 
+                stroke-width=".07" 
+                x="0" 
+                y="0" 
+                width={ROBOT_WIDTH} 
+                height={ROBOT_WIDTH} 
+                transform={`translate(${robotPose.x - ROBOT_WIDTH / 2},${robotPose.y - ROBOT_WIDTH / 2}) rotate(${robotPose.angle}, ${ROBOT_WIDTH / 2}, ${ROBOT_WIDTH / 2})`} />
+            <line 
+                x1={robotPose.x  - ROBOT_WIDTH / 2 + .8} 
+                y1={robotPose.y} 
+                x2={robotPose.x} 
+                y2={robotPose.y} 
+                stroke={!isGhost ? "rgb(255,0,0)" : "rgb(155,0,0)"} 
+                style="stroke-width:0.08" 
+                transform={`rotate(${robotPose.angle}, ${robotPose.x}, ${robotPose.y})`} />
+        </>
+    }
+
+    render() {
+        this.robotPose = this.decodeRobotPoseArray(this.props.nt[this.robotPoseKey])
+
+        const ghostEnabled = this.props.nt[this.ghostEnableKey] ?? false
+
+        if (ghostEnabled) this.ghostPose = this.decodeRobotPoseArray(this.props.nt[this.ghostPoseKey])
 
         return (
             <div className="flex items-center justify-center h-screen">
@@ -29,23 +59,11 @@ export default class Field extends Component {
                 <svg viewBox={`0 0 ${FIELD_WIDTH} ${FIELD_HEIGHT}`} className="w-4/6 max-h-3/4" transform="scale(1, -1)" >
                     <rect style="fill:black;" width={FIELD_WIDTH} height={FIELD_HEIGHT} />
                     <circle stroke="white" stroke-width=".05" cx={FIELD_WIDTH / 2} cy={FIELD_HEIGHT / 2} r={TARGET_RADIUS} />
-                    <rect 
-                        stroke="white" 
-                        stroke-width=".07" 
-                        x="0" 
-                        y="0" 
-                        width={ROBOT_WIDTH} 
-                        height={ROBOT_WIDTH} 
-                        transform={`translate(${this.robotPose.x - ROBOT_WIDTH / 2},${this.robotPose.y - ROBOT_WIDTH / 2}) rotate(${this.robotPose.angle}, ${ROBOT_WIDTH / 2}, ${ROBOT_WIDTH / 2})`} />
-                    <line 
-                        x1={this.robotPose.x  - ROBOT_WIDTH / 2 + .8} 
-                        y1={this.robotPose.y} 
-                        x2={this.robotPose.x} 
-                        y2={this.robotPose.y} 
-                        style="stroke:rgb(255,0,0);stroke-width:0.08" 
-                        transform={`rotate(${this.robotPose.angle}, ${this.robotPose.x}, ${this.robotPose.y})`} />
-                    <polyline points={this.pathPoints} stroke='red' stroke-width='.08' />
 
+                    {this.createRobotSvg(this.robotPose, false)}
+                    {ghostEnabled ? this.createRobotSvg(this.ghostPose, true) : <></>}
+                    
+                    <polyline points={this.pathPoints} stroke='red' stroke-width='.08' />
                 </svg>
             </div>
         )
