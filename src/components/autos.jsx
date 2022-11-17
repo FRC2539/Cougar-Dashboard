79914cad539ce69bfe2e0e990e41f62a2700851a
Dominic Ruggiero <dominic@userexe.me>
2022-11-16 20:44:02 -0500

    loaders

--- src/components/autos.jsx
+++ src/components/autos.jsx
@@ -24,18 +24,35 @@ export default class Autos extends Component {
     }
 
     getButtonHTML() {
-        if(this.props.nt == null || this.props.nt[this.autos] == null) return <span></span> 
+        if(this.props.nt == null || this.props.nt[this.autos] == null) return []
         
         return this.props.nt[this.autos].map((value, i) => {
             return <a onClick={() => this.setCurrentAuto(value)} class={this.getButtonColorClass(value) + " cursor-pointer p-1 text-center text-xs border border-gray break-all flex justify-center items-center " + this.getBorderRadiusClass(this.props.nt[this.autos], i)}><p>{value}</p></a>
         })
     }
 
+
     render() {
+        let buttonHTML = this.getButtonHTML()
+        let body = <></>
+        if (buttonHTML.length != 0) {
+            body = (
+                <div class="grid grid-cols-3 col-span-2 row-span-2 mt-2 ml-2">
+                {buttonHTML}
+                </div>
+                )
+        } else {
+            body = (
+                <div className="w-full h-full loader flex justify-center space-x-1 anima pt-3">
+                  <div className="flex-auto special-gray w-5 h-8 rounded-md"></div>
+                  <div className="flex-auto special-gray w-5 h-8 rounded-md"></div>
+                  <div className="flex-auto special-gray w-5 h-8 rounded-md"></div>
+                </div>
+              );
+        }
+
         return (
-            <div class="grid grid-cols-3 col-span-2 row-span-2 mt-2 ml-2">
-                {this.getButtonHTML()}
-            </div>
+                body
         )
     }
 }
--- src/debug.jsx
+++ src/debug.jsx
@@ -1,34 +1,59 @@
-import { Component } from "preact"
-import DebugHeader from "./components/debug-header"
+import { Component } from "preact";
+import DebugHeader from "./components/debug-header";
 
 export default class Debug extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            search: "",
-        }
+    this.state = {
+      search: "",
+    };
+  }
+
+  render() {
+    const html = this.dataToHTML(this.props.ntMap);
+    let body = <></>;
+    if (html.length != 0) {
+      let body = (
+        <div className="bg-gray h-full py-3 px-4">
+          <div className="h-full overflow-y-auto overflow-x-hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-2">
+            {html}
+          </div>
+        </div>
+      );
+    } else {
+      body = (
+        <div className="grid grid-cols-3 grid-rows-3 loader h-full w-full gap-6 p-6">
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray h-full w-full rounded-md"></div>
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray h-full w-full rounded-md"></div>
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray w-full h-full rounded-md"></div>
+          <div className="special-gray h-full w-full rounded-md"></div>
+        </div>
+      );
+    }
+    return body;
+  }
+
+  dataToHTML(data) {
+    const html = [];
+
+    for (const [key, value] of data) {
+      html.push(
+        <DebugHeader
+          ntMap={this.props.ntMap}
+          nt={this.props.nt}
+          title={key}
+          data={value}
+          putValueNT={this.props.putValueNT}
+        />
+      );
     }
 
-    render() {
-        const html = this.dataToHTML(this.props.ntMap)
-        
-        return (
-            <div className="bg-gray h-full py-3 px-4">
-                <div className="h-full overflow-y-auto overflow-x-hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-2">
-                    {html}
-                </div>
-            </div>
-        )
-    }
-
-    dataToHTML(data) {
-        const html = []
-
-        for(const [key, value] of data) {
-            html.push(<DebugHeader ntMap={this.props.ntMap} nt={this.props.nt} title={key} data={value} putValueNT={this.props.putValueNT} />)
-        }
-
-        return html
-    } 
+    return html;
+  }
 }
