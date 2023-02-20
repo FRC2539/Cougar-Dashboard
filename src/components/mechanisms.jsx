904e7575c7b9183e9026c7e82cf1874a950acc8d
Weaver Goldman <we.goldm@gmail.com>
2023-02-20 08:48:52 -0500

    Added correct keys for testing.

--- src/components/mechanisms.jsx
+++ src/components/mechanisms.jsx
@@ -5,18 +5,10 @@ export default class Mechanisms extends Component {
         super(props)
 
         this.driveTemperaturesKey = "/SwerveDriveSubsystem/Drive Temperatures"
-
-        // this.shooterRPM1 = "/Shooter/Shooter RPM"
-        // this.shooterRPM2 = "/Shooter/Shooter RPM2"
-
-        // this.hoodPosition = "/Hood/Hood Position"
-
-        // this.conveyorBall = "/BallSystem/Conveyor Ball"
-        // this.chamberBall = "/BallSystem/Chamber Ball"
-
-        // this.mechanisms = {
-        //     "ML Target": "/ML/targetAcquired"
-        // }
+        this.hasGamePieceKey = "/Robot/Has Game Piece"
+        this.pressureKey = "/Robot/Pressure"
+        this.minPressureKey = "/Robot/Minimum Pressure"
+        this.secondsSinceVisionUpdateKey = "/VisionSubsystem/Last Update"
     }
 
     average(array) {
@@ -29,10 +21,18 @@ export default class Mechanisms extends Component {
         return sum / array.length
     }
 
+    round(num, decimalPlaces = 0) {
+        var p = Math.pow(10, decimalPlaces);
+        var n = (num * p) * (1 + Number.EPSILON);
+        return Math.round(n) / p;
+    }
+
     render() {
         const driveMotorTemps = this.props.nt[this.driveTemperaturesKey] ?? []
         const averageMotorTemp = this.average(driveMotorTemps)
         const motorTempThreshold = 70;
+
+        const lastVisionUpdate = this.round(this.props.nt[this.secondsSinceVisionUpdateKey], 2)
 
         return (
             <div className="shadow bg-orange col-span-4 row-span-2 rounded-lg p-2 mt-1">
@@ -47,19 +47,19 @@ export default class Mechanisms extends Component {
 
                 <div className="flex flex-row font-medium justify-between items-center mb-2">
                     <div className="text-left self-start">Has Game Piece:</div>
-                    <div className={"text-right w-1/4 h-7 rounded-md border-black border " + (false ? "bg-green" : " bg-black")}></div>
+                    <div className={"text-right w-1/4 h-7 rounded-md border-black border " + (this.props.nt[this.hasGamePieceKey] ? "bg-green" : " bg-black")}></div>
                 </div>
 
                 <div className="flex flex-row font-medium justify-between items-center mb-2">
                     <div className="text-left self-start">Is Under Pressure:</div>
-                    <div className={"text-right w-1/4 h-7 rounded-md border-black border " + (false ? "bg-purple" : " bg-black")}></div>
+                    <div className={"text-right w-1/4 h-7 rounded-md border-black border " + (this.props.nt[this.pressureKey] < this.props.nt[this.minPressureKey] ? "bg-purple" : " bg-black")}></div>
                 </div>
 
                 <hr className="mb-2"></hr>
 
                 <div className="flex flex-row font-medium justify-between items-center mb-2">
-                    <div className="text-left self-start">Vision Updated:</div>
-                    <div className="text-right self-end">0 seconds ago</div>
+                    <div className="text-left self-start text-sm">Vision Updated:</div>
+                    <div className="text-right self-end text-sm">{lastVisionUpdate} seconds ago</div>
                 </div>
             </div>
         )
