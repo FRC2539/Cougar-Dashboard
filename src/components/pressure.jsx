40e8c18f38e3aef29bb5b4fd6d224e198c25d58a
li-pearl <anshipaul@outlook.com>
2023-03-11 11:56:17 -0500

    Styled and fixed the pressure widget.

--- src/components/pressure.jsx
+++ src/components/pressure.jsx
@@ -11,21 +11,25 @@ export default class Pressure extends Component {
     }
 
     render() {
-        const minPressure = this.props.nt[this.minPressureKey]
+        // const minPressure = this.props.nt[this.minPressureKey]
+        const minPressure = 80
 
-        let pressure = this.props.nt[this.pressureKey]
+
+        // let pressure = this.props.nt[this.pressureKey]
+        let pressure = 100
+
         let percentPressurized = Math.round((pressure/maxPressure) * 100)
 
-        let minPressurePercentleft = Math.round(((minPressure/maxPressure) * 100) - percentPressurized)
+        let minPressurePercent = Math.round((minPressure/maxPressure) * 100)
         
         return (
             <div className="shadow row-span-1 w-11/12 col-span-4 bg-orange rounded-lg p-1 mt-3 mb-3 grid grid-cols-1">
                 <p className="text-lg font-bold mb-1">Pressure</p>
                 <hr className="mb-2"></hr>
 
-                <div className="overflow-hidden h-6 mb-4 text-xs flex rounded-full bg-white">
-                    <div style={{ width: percentPressurized + "%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-black justify-center bg-purple">{percentPressurized + "%"}</div>
-                    <div style={{width: minPressurePercentleft + "%"}} className="shadow-none flex flex-col text-center whitespace-nowrap justify-center bg-purple-light"></div>
+                <div className="overflow-hidden h-6 mb-4 relative z-0 text-xs flex rounded bg-white border-black border">
+                    <div style={{ width: percentPressurized + "%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-black justify-center absolute inset-y-0 left-0 z-5 bg-purple">{pressure + " psi"}</div>
+                    <div style={{width: minPressurePercent + "%"}} className="shadow-none flex flex-col text-center whitespace-nowrap justify-center absolute inset-y-0 left-0 z-10 border-r-2 border-black"></div>
                 </div>
             </div>
         )
