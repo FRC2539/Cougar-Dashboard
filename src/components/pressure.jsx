f45d8117ce8a2d4c04a8759a0bb73d6b05b5bf5d
Weaver Goldman <we.goldm@gmail.com>
2023-03-13 21:33:43 -0400

    Looks pretty good.

--- src/components/pressure.jsx
+++ src/components/pressure.jsx
@@ -12,7 +12,11 @@ export default class Pressure extends Component {
 
     render() {
         const minPressure = this.props.nt[this.minPressureKey]
+        // const minPressure = 100
+
         const pressure = this.props.nt[this.pressureKey]
+        // const pressure = 110
+
         const percentPressurized = (pressure/maxPressure) * 100
         const minPressurePercent = (minPressure/maxPressure) * 100
         
@@ -21,11 +25,11 @@ export default class Pressure extends Component {
                 <p className="text-lg font-bold mb-1">Pressure</p>
                 <hr className="mb-2"></hr>
 
-                <div className="overflow-hidden h-8 relative mb-1 z-0 text-s flex rounded bg-white border-black border-r-2 border-2">
-                    <div style={{width: percentPressurized + "%"}} className="shadow-none flex flex-col text-left whitespace-nowrap text-white justify-center absolute inset-y-0 left-0 z-1 border-r-2 bg-purple">
+                <div className="h-8 relative mb-1 z-0 text-s flex rounded bg-white border-black border-r-2 border-2">
+                    <div style={{width: percentPressurized + "%"}} className="shadow-none flex flex-col h-full text-left whitespace-nowrap text-white justify-center absolute inset-y-0 left-0 z-1 bg-purple rounded-l">
                         <p className="pl-3">{Math.round(pressure) + " psi"}</p>
                     </div>
-                    <div style={{width: minPressurePercent + "%"}} className="shadow-none flex flex-col text-center whitespace-nowrap justify-center absolute inset-y-0 left-0 z-2 border-r-2 border-black"></div>
+                    <div style={{width: minPressurePercent + "%"}} className="shadow-none flex flex-col h-full text-center whitespace-nowrap justify-center absolute inset-y-0 left-0 z-2 border-r-2 rounded-none border-black"></div>
                 </div>
             </div>
         )
